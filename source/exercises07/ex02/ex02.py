ab01d6af964a87890fcfb8730cb2f1290ca2fee0
lorenzovolpi <yoyopro@gmail.com>
2020-01-05 12:42:54 +0100

    Update solutions

--- source/exercises07/ex02/ex02.py
+++ source/exercises07/ex02/ex02.py
@@ -1,4 +1,12 @@
+def fib_gen(n):
+    f1 = 1
+    f2 = 1
+    for _ in range(n):
+        yield f1
+        f1, f2 = f2, f1 + f2
+
 
 if __name__ == "__main__":
     import sys
-    
+    for i in fib_gen(int(sys.argv[1])):
+        print(i)
