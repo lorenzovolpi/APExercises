346b0628062f2bca00902ea226426a764f8291c3
lorenzovolpi <yoyopro@gmail.com>
2020-01-17 17:09:55 +0100

    Update solutions

--- source/assignment2/part3/benchmark.py
+++ source/assignment2/part3/benchmark.py
@@ -1,6 +1,8 @@
 import time
 import csv
 import threading
+import requests
+import os
 
 def benchmark(_func=None, *, warmups=0, iter=1, verbose=False, csv_file=None):
     def benchmark_dec(f):
@@ -77,6 +79,34 @@ def th_aux(nthreads, fibo_par):
     for th in ths:
         th.join()
 
+def loadAndExec(url):
+    if(url == ""): 
+        return
+
+    tmp = "tmp" + str(time.time()) + ".py"
+    r = requests.get(url)
+    with open(tmp, "w") as f:
+        f.write(r.text)
+    
+    os.system("python3 " + tmp)
+    os.remove(tmp)
+
+def prepost(_func=None, *, preurl="", posturl=""):
+    def prepost_dec(f):
+        def prepost_wrap(*args, **kwargs):
+            loadAndExec(preurl)
+            f(*args, **kwargs)
+            loadAndExec(posturl)
+        
+        return prepost_wrap
+    
+    if _func is None:
+        return prepost_dec
+    else:
+        return prepost_dec(_func)
+
+
+@prepost(preurl="http://pages.di.unipi.it/corradini/Didattica/AP-19/PROG-ASS/02/pre.py", posturl="http://pages.di.unipi.it/corradini/Didattica/AP-19/PROG-ASS/02/post.py")
 def test(fibo_par):
 
     #f-1-16
@@ -95,8 +125,11 @@ def test(fibo_par):
     f_8_2 = benchmark(th_aux, iter=2, csv_file="f-8-2.csv")
     f_8_2(8, fibo_par)
 
+
+
 if __name__ == "__main__":
     test(30)
+
 
 
 # Concretamente non si osserva un apparente vantaggio nella parallelizzazione
